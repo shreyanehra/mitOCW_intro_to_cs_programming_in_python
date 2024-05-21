48ca3f9680720cf598be9684553aeef60cc7c0e4
shreyanehra <f20200146@goa.bits-pilani.ac.in>
2024-05-21 17:01:24 +0530

    adding part c

--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -1,36 +1,39 @@
-
-
 annual_salary = int(input("Enter your annual salary in rupees : "))
 annual_rate_of_interest = 0.04
-semi_annual_salary_increase  = 0.07
+semi_annual_salary_increase = 0.07
 num_months = 36
 low = 0
 high = 10000
-money_2_save = 1000000
-guessed_savings_rate = high/10000
+money_2_save = 1000000*0.25
 epsilon = 100
-savings_now = 0
 
 def current_savings(savings, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, guessed_savings_rate, num_months):
-    for i in range(0, 36, 1):
-        if i%6==0 and i!=0:
-            annual_salary += annual_salary*semi_annual_salary_increase
-        savings = savings(1 + (annual_rate_of_interest/12)) + (annual_salary/12)*guessed_savings_rate
+    for i in range(num_months):
+        if i % 6 == 0 and i != 0:
+            annual_salary += annual_salary * semi_annual_salary_increase
+        savings += (annual_salary / 12) * guessed_savings_rate + savings*(annual_rate_of_interest / 12)
         
     return savings
-        
-#if all of the sary was gettings saved
-savings_now = current_savings(savings_now, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, guessed_savings_rate, num_months) 
 
-def canBuy(savings_now, money_2_save):
-    if(savings_now<money_2_save) :
-        print("Can't buy the house in 3 months.")
-        return False
-    else:
-        return True
+savings_now = current_savings(0, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, 1.00, num_months)
+canBuy = savings_now >= money_2_save
+if canBuy == False:
+    print ("Can't buy house")
+else : 
+    guessed_savings_rate = (high + low) / 20000
+    savings_now = current_savings(0, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, guessed_savings_rate, num_months)
+
+    steps_in_bisection = 1
+    while abs(money_2_save - savings_now) > epsilon and canBuy:
+        if money_2_save > savings_now:
+            low = guessed_savings_rate
+        else:
+            high = guessed_savings_rate
+
+        guessed_savings_rate = (high + low) / 2
+        savings_now = current_savings(0, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, guessed_savings_rate, num_months)
+        steps_in_bisection += 1
+
+    print("Steps in Bisection :", steps_in_bisection)
+    print("Guessed rate :", guessed_savings_rate)
     
-guessed_savings_rate = int((high+low)/20000)
-savings_now = current_savings(0, annual_salary, annual_rate_of_interest, semi_annual_salary_increase, guessed_savings_rate, num_months) 
-
-while(abs(money_2_save - savings_now)>=epsilon)
-    if(money_2_a
